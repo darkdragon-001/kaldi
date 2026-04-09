be2351fb9c625b7687ed9ee88f7c2687b665bbfa
Dark Dragon <darkdragon-001@web.de>
2026-04-09 13:26:47 +0200

    Fix arm64 build

--- tools/extras/install_openblas_clapack.sh
+++ tools/extras/install_openblas_clapack.sh
@@ -9,7 +9,7 @@ git clone -b v${CLAPACK_VERSION} --single-branch https://github.com/alphacep/cla
 make -C OpenBLAS ONLY_CBLAS=1 DYNAMIC_ARCH=1 TARGET=NEHALEM USE_LOCKING=1 USE_THREAD=0 NUM_THREADS=512 all
 make -C OpenBLAS PREFIX=$(pwd)/OpenBLAS/install install
 mkdir -p clapack/BUILD && cd clapack/BUILD && cmake .. \
-    && make -j 10 -C F2CLIBS \
-    && make -j 10 -C BLAS \
-    && make -j 10 -C SRC \
+    && make -j $(nproc) -C F2CLIBS \
+    && make -j $(nproc) -C BLAS \
+    && make -j $(nproc) -C SRC \
     && find . -name "*.a" | xargs cp -t ../../OpenBLAS/install/lib
